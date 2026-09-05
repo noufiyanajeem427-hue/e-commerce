5724a505a9690ce92140ba6b772a8bd4df4289d3
ayan726678-svg <ayan726678@gmail.com>
2026-09-05 18:53:25 +0530

    Update OwnerContext.jsx

--- ecommerce-shop-owner/src/context/OwnerContext.jsx
+++ ecommerce-shop-owner/src/context/OwnerContext.jsx
@@ -1,9 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 
 const OwnerContext = createContext();
-
-
-
 export const OwnerProvider = ({ children }) => {
   const [store, setStore] = useState({
     name: 'My Shop',
@@ -11,14 +8,12 @@ export const OwnerProvider = ({ children }) => {
     orders: 328,
     products: 142,
   });
-
   return (
     <OwnerContext.Provider value={{ store, setStore }}>
       {children}
     </OwnerContext.Provider>
   );
 };
-
 export const useOwner = () => useContext(OwnerContext);
 
 export default OwnerContext;
